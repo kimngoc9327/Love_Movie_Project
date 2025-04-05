3568cb75dcb9eccafb6b3efcd13c5acc43116dc9
kimngoc9327 <kimngoc9327tn@gmail.com>
2025-04-05 21:49:22 +0700

    feat: add responsive design for MovieFilter page

--- src/components/ui/PlayTrailerButton.jsx
+++ src/components/ui/PlayTrailerButton.jsx
@@ -4,7 +4,7 @@ function PlayTrailerButton({ onClick }) {
   return (
     <div
       onClick={onClick}
-      className="flex space-x-2 w-38 justify-center items-center px-3 py-1 bg-red-500 hover:cursor-pointer"
+      className="flex space-x-2 w-38 justify-center items-center px-3 py-1 bg-red-500 cursor-pointer max-md:text-sm"
     >
       <i className="ri-play-large-line"></i>
       <p>PLAY TRAILER</p>
